d29a48ec9d9df96361f13779481e7a0c086f3ca0
Chaofeng <Z.Chaofeng94@gmail.com>
2018-04-28 22:47:26 -0400

    repair sh issue

--- bin/json.sh
+++ bin/json.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -rf ../data
-mkdir ../data
-../node_modules/solidity-parser/cli.js $1 > ../data/ast.json
+rm -rf ./data
+mkdir ./data
+./node_modules/solidity-parser/cli.js $1 > ./data/ast.json
